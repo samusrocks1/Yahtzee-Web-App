5a3d312036ca0eb595616aaf632c5f997335112b
Willy <wtaylor1992@gmail.com>
2021-09-25 01:31:34 -0400

    Added functions for computing the upper categories.

--- public_html/script.js
+++ public_html/script.js
@@ -17,33 +17,144 @@ let die4Hold = false;
 let die5Hold = false;
 let roll = 1;
 let round = 1;
-
-function game() {
-
-}
+//Declares variables for the upper row scores. upperCategory[0] corresponds to 1, etc.
+let upperCategory = [0, 0, 0, 0, 0, 0]; 
+let bonus = 0;
+/*Declares variables for lower row. 
+ * Slot 0 = Three of a kind.
+ * Slot 1 = Four of a kind.
+ * Slot 2 = Full House.
+ * Slot 3 = Small Straight.
+ * Slot 4 = Large Straight.
+ * Slot 5 = Chance
+ * Slot 6 = Yahtzee
+ */
+let lowerCategory = [0, 0, 0, 0, 0, 0, 0];
 
 function useOnes() {
-
+    if (die1 === 1) {
+        upperCategory[0] = upperCategory[0] + 1;
+    }
+    if (die2 === 1){
+        upperCategory[0] = upperCategory[0] + 1;
+    }
+    if (die3 === 1) {
+        upperCategory[0] = upperCategory[0] + 1;
+    }
+    if (die4 === 1){
+        upperCategory[0] = upperCategory[0] + 1;
+    }
+    if (die5 === 1){
+        upperCategory[0] = upperCategory[0] + 1;
+    }
+    const onesBtn = document.getElementById("onesBtn");
+    onesBtn.parentNode.removeChild(onesBtn);
+    checkBonus();
 }
 
 function useTwos() {
-
+    if (die1 === 2) {
+        upperCategory[1] = upperCategory[1] + 2;
+    }
+    if (die2 === 2){
+        upperCategory[1] = upperCategory[1] + 2;
+    }
+    if (die3 === 2) {
+        upperCategory[1] = upperCategory[1] + 2;
+    }
+    if (die4 === 2){
+        upperCategory[1] = upperCategory[1] + 2;
+    }
+    if (die5 === 2){
+        upperCategory[1] = upperCategory[1] + 2;
+    }
+    const twosBtn = document.getElementById("twosBtn");
+    twosBtn.parentNode.removeChild(twosBtn);
+    checkBonus();
 }
 
 function useThrees() {
-
+    if (die1 === 3) {
+        upperCategory[2] = upperCategory[2] + 3;
+    }
+    if (die2 === 3){
+        upperCategory[2] = upperCategory[2] + 3;
+    }
+    if (die3 === 3) {
+        upperCategory[2] = upperCategory[2] + 3;
+    }
+    if (die4 === 3){
+        upperCategory[2] = upperCategory[2] + 3;
+    }
+    if (die5 === 3){
+        upperCategory[2] = upperCategory[2] + 3;
+    }
+    const threesBtn = document.getElementById("threesBtn");
+    threesBtn.parentNode.removeChild(threesBtn);
+    checkBonus();
 }
 
 function useFours() {
-
+    if (die1 === 4) {
+        upperCategory[3] = upperCategory[3] + 4;
+    }
+    if (die2 === 4){
+        upperCategory[3] = upperCategory[3] + 4;
+    }
+    if (die3 === 4) {
+        upperCategory[3] = upperCategory[3] + 4;
+    }
+    if (die4 === 4){
+        upperCategory[3] = upperCategory[3] + 4;
+    }
+    if (die5 === 4){
+        upperCategory[3] = upperCategory[3] + 4;
+    }
+    const foursBtn = document.getElementById("foursBtn");
+    foursBtn.parentNode.removeChild(foursBtn);
+    checkBonus();
 }
 
 function useFives() {
-
+    if (die1 === 5) {
+        upperCategory[4] = upperCategory[4] + 5;
+    }
+    if (die2 === 5){
+        upperCategory[4] = upperCategory[4] + 5;
+    }
+    if (die3 === 5) {
+        upperCategory[4] = upperCategory[4] + 5;
+    }
+    if (die4 === 5){
+        upperCategory[4] = upperCategory[4] + 5;
+    }
+    if (die5 === 5){
+        upperCategory[4] = upperCategory[4] + 5;
+    }
+    const fivesBtn = document.getElementById("fivesBtn");
+    fivesBtn.parentNode.removeChild(fivesBtn);
+    checkBonus();
 }
 
 function useSixes() {
-
+    if (die1 === 6) {
+        upperCategory[5] = upperCategory[5] + 6;
+    }
+    if (die2 === 6){
+        upperCategory[5] = upperCategory[5] + 6;
+    }
+    if (die3 === 6) {
+        upperCategory[5] = upperCategory[5] + 6;
+    }
+    if (die4 === 6){
+        upperCategory[5] = upperCategory[5] + 6;
+    }
+    if (die5 === 6){
+        upperCategory[5] = upperCategory[5] + 6;
+    }
+    const sixesBtn = document.getElementById("sixesBtn");
+    sixesBtn.parentNode.removeChild(sixesBtn);
+    checkBonus();
 }
 
 function useThreeOfAKind() {
@@ -67,13 +178,36 @@ function useLargeStraight() {
 }
 
 function useChance() {
-
+    lowerCategory[5] = die1 + die2 + die3 + die4 + die5;
+    const chanceBtn = document.getElementById("chanceBtn");
+    chanceBtn.parentNode.removeChild(chanceBtn);
 }
 
 function useYahtzee() {
-
+    if (die1 === die2 && die2 === die3 && die3 === die4 && die4 === die5){
+        lowerCategory[6] = lowerCategory[6] + 50;
+    }
+    else{
+        const yahtzeeBtn = document.getElementById("yahtzeeBtn");
+        yahtzeeBtn.parentNode.removeChild(yahtzeeBtn);
+    }
 }
 
+function checkBonus(){
+    let counter = 0;
+    let subtotal = 0;
+    do{
+        subtotal = subtotal + upperCategory[counter];
+    } while (counter < upperCategory.length);
+    if (subtotal >= 63){
+        bonus = 35;
+    }
+    else {
+        bonus = 0;
+    }
+}
+
+//Rolls the dice and outputs the results.                      
 function rollDice() {
     //Rolls the first die if the player is not holding it.
     if (die1Hold === false) {
